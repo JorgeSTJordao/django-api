fe15b59f27240dd3bc0f22a285befca0ac768c51
Jorge Samuel Teixeira Jordão <116199476+JorgeSTJordao@users.noreply.github.com>
2025-04-21 03:22:04 -0300

    feat: the rest of commits

--- apiproject/api/views/__init__.py
+++
@@ -1,3 +0,0 @@
-from .users import get_user, create_user, user_detail
-
-__all__ = ['get_user', 'create_user', 'user_detail']
--- apiproject/api/views/users/__init__.py
+++
@@ -1,5 +0,0 @@
-from .list import get_user
-from .create import create_user
-from .detail import user_detail
-
-__all__ = ['get_user', 'create_user', 'user_detail'] 
